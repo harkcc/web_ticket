5a2ee4b19417c1c01a5efcb7a62c4afdd33dcad1
harkcc <harkcc@users.noreply.github.com>
2026-07-27 14:45:07 +0800

    fix: update LingXing login double-check flow

--- login.py
+++ login.py
@@ -1,20 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2023/3/27 10:49
+@Auth ： jax
+@E-mail: 2567040897@qq.com
+@File ：login.py
+@IDE ：PyCharm
+"""
 import base64
+
 import requests
+import json
 from Crypto.Cipher import AES
-from Crypto.Util.number import getPrime, isPrime
-from Crypto.Util.strxor import strxor
-
-
-# ACCOUNT = "13391234626bt"
-# PWD = '0JUER4'
-
+from Crypto.Util.number import *
+from Crypto.Util.strxor import *
+from Crypto.Util.Padding import pad
+#
 ACCOUNT = "baitai-350000"
-PWD = 'Lx159357'
+PWD = 'Lx1593572'
 
-PROXIES = {
-    'http': 'http://remake:123456@175.6.58.222:2021',
-    'https': 'http://remake:123456@175.6.58.222:2021',
-}
 
 def get_login_secretkey():
     url = "https://gw.lingxingerp.com/newadmin/api/passport/getLoginSecretKey"
@@ -38,7 +41,10 @@ def get_login_secretkey():
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
         'x-ak-company-id': '901217529031491584',
+        'x-ak-request-id': '5abadf08-a70b-4e53-892a-7f5b0aacef8f',
         'x-ak-request-source': 'erp',
+        'x-ak-version': '1.0.0.0.0.200',
+        'x-ak-zid': ''
     }
 
     response = requests.request("POST", url, headers=headers, data=payload).json()
@@ -81,7 +87,7 @@ def encrypt_aes(plaintext, key):
     return base64.b64encode(ciphertext).decode()
 
 
-def login(pwd,secretId):
+def login(pwd,secretId,login_ticket = None):
     headers = {
         'authority': 'gw.lingxingerp.com',
         'accept': 'application/json, text/plain, */*',
@@ -113,9 +119,25 @@ def login(pwd,secretId):
         'auto_login': 1,
         # 'randStr': '@ywL',
         # 'ticket': 't03hPBBq3jECZDNi0KLbw71udaHFI0LKOu8CMO75Jz4Dd16-8FlGsO6aDfXMLND3mixktpRm4DgpHAxeVIvvWY6dFcZycj0MRd_okhxHlslv_dFfPY6ShZD1w**',
+
+
         'sensorsAnonymousId': '18725ea3f9fd58-07aaf295e0ad0dc-26021151-1764000-18725ea3fa0804',
         'secretId': secretId,
     }
+    if login_ticket:
+        json_data['doubleCheckLoginReq'] = {
+            "doubleCheckType": 1,
+            "loginTick": login_ticket,
+            "mobileLoginCode": "",
+            "bindMobile": "",
+            "bindNationCode": "86",
+            "skipBindMfaDevice": True
+        }
+    # else:
+    #     json_data.update({
+    #         "randStr": "@WTB",
+    #         "ticket": "tr03wogzWKY-iMLteWL6xX2v525f2uAOVb26RLzbMp5BzpLdSU8i9zSpQj-i0Ge-0xVv7Ox17uoK_OGQQACe6yGeG1PoL8HoCJmqGgPTGmVBJSX8Tm-9L2IerOlOlqEvDR5LbxKeRndNms0*",
+    #     })
 
     response = requests.post('https://gw.lingxingerp.com/newadmin/api/passport/login', headers=headers, json=json_data).json()
     return response
@@ -126,19 +148,28 @@ def login(pwd,secretId):
 
 
 def run():
-    login_secretkey = get_login_secretkey()
-    # print(login_secretkey)
-    key = login_secretkey['secretKey']
-    secretId = login_secretkey['secretId']
-    # key = 'mVTZ8pXTQXsEBwFw'.encode('utf-8')
-    data = PWD
-    pwd = encrypt_aes(data,key)
-    # print(pwd)
-    res = login(pwd,secretId)
 
-    # print(res)
+    login_ticket = None
+
+    for i in range(2):
+        login_secretkey = get_login_secretkey()
+        # print(login_secretkey)
+        key = login_secretkey['secretKey']
+        secretId = login_secretkey['secretId']
+        # key = 'mVTZ8pXTQXsEBwFw'.encode('utf-8')
+        data = PWD
+        pwd = encrypt_aes(data,key)
+        # print(pwd)
+        res = login(pwd,secretId,login_ticket)
+        login_ticket = res.get("doubleCheckConfigRes", {}).get("loginTicket", None)
+
+    print(res)
     return res['token']
 
 
-# if __name__ == '__main__':
-#     print(run())
+
+
+
+
+if __name__ == '__main__':
+    print(run())
